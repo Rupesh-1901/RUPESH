d7e8b41e8a1fba97096526b21eae2611af78ae99
Rupesh-1901 <rupeshmishra954@gmail.com>
2020-05-08 22:37:12 +0530

    Task 0, full code

---
+++ app/src/main/java/com/example/internship/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.internship.R.layout.activity_main;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
